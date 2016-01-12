b185ea7344a38f0fde0a61dd40da4f06f8e5cf16
Enrico Teotti <enrico.teotti@gmail.com>
2016-01-12 10:07:54 -0700

    Fix test by requiring drug_information class from gem entry point.

--- local_gems/drug_information/lib/drug_information.rb
+++ local_gems/drug_information/lib/drug_information.rb
@@ -1,4 +1,5 @@
 require "drug_information/version"
+require "drug_information/fetcher"
 
 module DrugInformation
   # Your code goes here...
---
+++ local_gems/drug_information/lib/drug_information/fetcher.rb
@@ -0,0 +1,10 @@
+module DrugInformation
+  class Fetcher
+    def initialize(subscriber_id)
+    end
+
+    def details
+      [ { name: 'Painkiller F332' } ]
+    end
+  end
+end
